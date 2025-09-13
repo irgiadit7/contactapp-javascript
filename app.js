31add3d424bc71dde1395bd099265dba3ca1dcf6
irgi <irgiaditpratama@gmail.com>
2025-09-14 00:44:19 +0700

    '

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const morgan = require('morgan');
 const app = express();
 const port = 3000;
+const {loadContact, findContact, addContact, cekDuplikat} = require('./utils/contacts');
+const {body, validationResult, check} = require ('express-validator');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
 //gunakan ejs
 app.set('view engine', 'ejs');
 
-//third-party middleware
-app.use(expressLayouts);
-app.use(morgan('dev'));
+app.use(expressLayouts); //third-party middleware
+app.use(express.static('public')); //built-in middleware
+app.use(express.urlencoded({extended: true}));
 
-//built-in middleware
-app.use(express.static('public'));
+//konfigurasi flash
+app.use(cookieParser('secret'));
+app.use(session({
+    cookie: {maxAge: 6000,
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    }
+}));
 
-//apllication level middleware
-app.use((req, res, next)=>{
-  console.log('Time: ', Date.now());
-  next();
-});
+app.use(flash());
 
 app.get('/', (req, res) => {
     const mahasiswa = [
@@ -51,14 +58,59 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
+    const contacts = loadContact(); 
     res.render('contact', {
         layout: 'layouts/main-layout',
-        title: 'Halaman Contact'
+        title: 'Halaman Contact',
+        contacts,
+        msg: req.flash('msg'),
     });
 });
 
-app.get('/product/:id', (req, res) => {
-    res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`);
+//halaman form tambah data contact
+app.get('/contact/add', (req, res)=> {
+    res.render('add-contact', {
+        title: 'form tambah data contact',
+        layout: 'layouts/main-layout',
+    })
+});
+
+//prosess data contact
+app.post('/contact', [
+    body('nama').custom((value)=>{
+        const duplikat = cekDuplikat(value);
+        if(duplikat) {
+            throw new Error ('Sudah terdaftar!');
+        }
+        return true;
+    }),
+    check('email', 'Email tidak valid!').isEmail(), 
+    check('nohp', 'No hp tidak valid!').isMobilePhone('id-ID')
+    ], (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        // return res.status(400).json({errors: errors.array()});
+        res.render('add-contact', {
+            title: 'form tambah data contact',
+            layout: 'layouts/main-layout',
+            errors: errors.array(),
+        });
+         }
+        else{
+             addContact(req.body);
+             //kirimkan flash massage
+             req.flash('msg', 'Data contact berhasil ditambahkan!');
+            res.redirect('/contact');
+        }
+});
+
+app.get('/contact/:nama', (req, res) => {
+    const contact = findContact(req.params.nama); 
+    res.render('detail', {
+        layout: 'layouts/main-layout',
+        title: 'Halaman Detail Contact',
+        contact,
+    });
 });
 
 app.use('/', (req, res) => {
